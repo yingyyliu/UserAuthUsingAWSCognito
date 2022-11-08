ad3bd26329ee9efc4d2ccddec09086fd9cc1f7ce
Yong Liu <yingyyliu@users.noreply.github.com>
2022-11-08 20:52:31 +1100

    Initial version

---
+++ UserAuthUsingAWSCognito.UnitTest/UserAuthUsingAWSCognito.Web/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UserAuthUsingAWSCognito.UnitTest.UserAuthUsingAWSCognito.Web.Controllers
+{
+    public class HomeControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
